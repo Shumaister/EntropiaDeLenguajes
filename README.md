3983b11419cc1e76aace715043b8894655179c5a
shumaister <gabrielmaiori@gmail.com>
2020-05-01 21:42:56 -0300

    Subo logica de calculo de propabilidades de ocurrencias, subo la tabla para mostrarlos y cambio el readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EntropiaDeLenguajes
+
+En esta pagina se calcula la cantidad de ocurrencias de disintos caracteres del abecedario, en ingles o español.
--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,168 @@
-function contar_palabras(){
-	//Obtenemos el texto del campo
-	var texto = document.getElementById("comment").value;
+function CrearDiccionario() {
+	return {
+		'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
+		'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
+		'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
+	}
+}
 
-	//Reemplazamos los saltos de linea por espacios
-	texto = texto.replace (/\r?\n/g," ");
+function procesarTodo() {
+	let diccionario = CrearDiccionario()
 
-	//Reemplazamos los espacios seguidos por uno solo
-	texto = texto.replace (/[ ]+/g," ");
+	//Quito el texto del campo
+	let texto = document.getElementById("comment").value
 
-	//Quitarmos los espacios del principio y del final
-	texto = texto.replace (/^ /,"");
-	texto = texto.replace (/ $/,"");
+	//Quito los saltos de linea por espacios
+	texto = texto.replace(/\r?\n/g, " ")
 
-	//Troceamos el texto por los espacios
-	var textoTroceado = texto.split (" ");
+	//Quito los espacios seguidos por uno solo
+	texto = texto.replace(/[ ]+/g, " ")
 
-	//Contamos todos los trozos de cadenas que existen
-	var numeroPalabras = textoTroceado.length;
-	
+	// Quito los espacios del principio y del final
+	texto = texto.replace(/^ /, "");
+	texto = texto.replace(/ $/, "");
+
+	// Pongo todo en minuscula 
+	texto = texto.toLowerCase()
+
+	// Solo contabilizo le la a - z . y quito al texto las palabras que no me importan
+	let textoSoloCaracteresValidos = ""
+	for (let c of texto) {
+		switch (c) {
+			case ' ': {
+				break
+			}
+			case 'a': {
+				diccionario[c]++
+				break
+			}
+			case 'b': {
+				diccionario[c]++
+				break
+			}
+			case 'c': {
+				diccionario[c]++
+				break
+			}
+			case 'd': {
+				diccionario[c]++
+				break
+			}
+			case 'e': {
+				diccionario[c]++
+				break
+			}
+			case 'f': {
+				diccionario[c]++
+				break
+			}
+			case 'g': {
+				diccionario[c]++
+				break
+			}
+			case 'h': {
+				diccionario[c]++
+				break
+			}
+			case 'i': {
+				diccionario[c]++
+				break
+			}
+			case 'j': {
+				diccionario[c]++
+				break
+			}
+			case 'k': {
+				diccionario[c]++
+				break
+			}
+			case 'l': {
+				diccionario[c]++
+				break
+			}
+			case 'm': {
+				diccionario[c]++
+				break
+			}
+			case 'n': {
+				diccionario[c]++
+				break
+			}
+			case 'o': {
+				diccionario[c]++
+				break
+			}
+			case 'p': {
+				diccionario[c]++
+				break
+			}
+			case 'q': {
+				diccionario[c]++
+				break
+			}
+			case 'r': {
+				diccionario[c]++
+				break
+			}
+			case 's': {
+				diccionario[c]++
+				break
+			}
+			case 't': {
+				diccionario[c]++
+				break
+			}
+			case 'u': {
+				diccionario[c]++
+				break
+			}
+			case 'v': {
+				diccionario[c]++
+				break
+			}
+			case 'w': {
+				diccionario[c]++
+				break
+			}
+			case 'x': {
+				diccionario[c]++
+				break
+			}
+			case 'y': {
+				diccionario[c]++
+				break
+			}
+			case 'z': {
+				diccionario[c]++
+				break
+			}
+			case 'ñ': {
+				if (esIngles)
+					continue
+
+				diccionario[c]++
+				break
+			}
+			default: {
+				continue
+			}
+		}
+		textoSoloCaracteresValidos = textoSoloCaracteresValidos + c
+	}
+
+	// Vuelvo a quitar espacios dobles
+	textoSoloCaracteresValidos = textoSoloCaracteresValidos.replace(/[ ]+/g, " ")
+
+	//Troceamos el texto por los espacios 
+	var textoTroceado = textoSoloCaracteresValidos.split(" ");
+	var numeroPalabras = textoTroceado.length; // Aca tenemos la cantidad de palabras
+
+	const cantidadLetras = textoSoloCaracteresValidos.length
+	for (const letra in diccionario) {
+		const prob = (diccionario[letra] * 100 / cantidadLetras).toFixed(4)
+		document.getElementById(`ocurrencia-${letra}`).textContent = prob + "%"
+	}
+
 	//Mostramos el número de palabras
 	alert(numeroPalabras);
 }
-
-// //Llamamos a la función para que cuente las palabras
-// contar_palabras();
